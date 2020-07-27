cfcff43cb2c42e58ff2ca7764891ad0ba2f64b0e
irene4848 <47068168+irene4848@users.noreply.github.com>
2020-07-27 18:50:18 -0500

    linear regression model

--- deflection_calc_dual.py
+++ deflection_calc_dual.py
@@ -6,7 +6,7 @@
 
 #  Date Created: 7/22/20
 
-#  Date Last Modified: 7/23/20
+#  Date Last Modified: 7/27/20
 
 from vars import *
 import sys
@@ -41,6 +41,7 @@ y = []
 for w in width:
     Inertia = (w * t**3)/12
     deflection = (Normal_Load*l**3)/(24*Emod*Inertia*inertia_factor)
+
     if (deflection >= deflection_min_vert) and (deflection <= deflection_max_vert):
         print("Deflection Criteria SATISFIED")
         print("Deflection: ",deflection/1e-3, 'mm')
@@ -73,11 +74,35 @@ print("  w: ",round(min_para[1]/1e-3,4), 'mm')
 print("  t: ",round(min_para[2]/1e-3,4), 'mm')
 print()
 
+# Linear Regression Analysis
+linReg_3D = LinearRegression()
+
+lin_x = np.array(x_fea_vert).reshape(-1,1)
+lin_y = np.array(y_fea_vert)
+
+# fits the linear regression
+linReg_3D.fit(lin_x, lin_y)
+# print results
+print('3D Model Regression')
+print('Coefficient of Determination: ',linReg_3D.score(lin_x,lin_y))
+print('Slope: ',linReg_3D.coef_)
+print('Intercept: ',linReg_3D.intercept_)
+print()
+
+# linear regression funcion 
+def lin_reg_func(x, m, b):
+    return m*x + b
+
+x_plot = np.arange(6,6.9,.1)
+
 #plot won't go through if samples don't meet input deflection criteria
+fig = plt.figure(figsize=(15,8))
 plt.plot(x,y,linestyle='--', marker='o', color='b',label='2D model results')
 plt.plot(x_fea_vert,y_fea_vert, linestyle='--', marker='x', color='r',label='3D FEA results')
+plt.plot(x_plot,lin_reg_func(x_plot,linReg_3D.coef_,linReg_3D.intercept_),label='3D Linear Regression')
+plt.text(6.7, .34, 'y = '+ str(round(linReg_3D.coef_[0],4)) +'x' + " + " + str(round(linReg_3D.intercept_,4)) +'\n$R^2$ = ' + str(round(linReg_3D.score(lin_x,lin_y),4)) , fontsize=15)
 plt.xlabel("Width (mm)")
 plt.ylabel("Deflection (mm)")
 plt.title('Vertical Beam Deflection with Changing Width')
 plt.legend()
-plt.show()     
+plt.show() 
